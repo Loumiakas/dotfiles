e1edc796cdcaf8e0e3852d2ec269dbe5cdfeb780
Ruslanas Laskovas <44226245+Loumiakas@users.noreply.github.com>
2024-11-19 12:21:39 +0000

    Update lsp.lua to use ts_ls instead of lspserver

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -12,7 +12,7 @@ require("lspconfig").clangd.setup {}
 -- Setup language servers.
 local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
-lspconfig.tsserver.setup {}
+lspconfig.ts_ls.setup {}
 lspconfig.rust_analyzer.setup {
   -- Server-specific settings. See `:help lspconfig-setup`
   settings = {
